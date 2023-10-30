78048745c500a301a3d8f53558ac95f2cacc1a70
Habil99 <abiyevhabil3@gmail.com>
2023-10-30 18:10:31 +0400

    chore: drop stock quantity column from product table

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,1 +1,13 @@
-export class CreateProductDto {}
+import { IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
+
+export class CreateProductDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(15)
+  @MaxLength(300)
+  description: string;
+}
